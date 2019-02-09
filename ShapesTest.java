4445b393887126fe06c772f0b9edf0b2002c7e04
Irene <35508977+guzman579@users.noreply.github.com>
2019-02-08 16:26:34 -0800

    Create ShapesTest.java

---
+++ ShapesTest.java
@@ -0,0 +1,26 @@
+// ShapesTest.java
+// Test application that displays class Shapes
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class ShapesTest
+{
+   public static void main(String[] args)
+   {
+      
+      String input = JOptionPane.showInputDialog(
+         "Enter 1 to draw rectangle\n" +
+         "Enter 2 to draw ovals");
+      
+      int choice = Integer.parseInt(input); 
+      
+      Shapes panel = new Shapes(choice);
+      
+      JFrame application = new JFrame(); // creates a new JFrame
+
+      application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      application.add(panel); 
+      application.setSize(300, 300); 
+      application.setVisible(true); 
+   }
+} // end class 
